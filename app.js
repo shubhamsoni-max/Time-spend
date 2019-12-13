24c8faefba85dd0ee19fe5d13b2aed61fa321ab4
Shubham Soni <chintusoni250796@gmail.com>
2019-12-13 22:15:31 +0530

    added

---
+++ app.js
@@ -0,0 +1,48 @@
+let button = document.querySelector('.button');
+let heading = document.querySelector('h1');
+let list = document.querySelector('ol');
+let para = document.querySelector('p');
+let seconds;
+let timer;
+let yourWork;
+let hour;
+let minutes;
+button.addEventListener('click', () => {
+
+  if (button.textContent === 'start') {
+    seconds = 0;
+    minutes = 0;
+    hour = 0;
+    yourWork = prompt('what are you going to do ?');
+    if (yourWork !== null && yourWork !== '') {
+      button.textContent = 'Done';
+      para.textContent = yourWork;
+      heading.textContent = `${hour}hr : ${minutes}min : ${seconds}sec`;
+      timer = setInterval(() => {
+
+        seconds++;
+
+        (minutes < 10) ? minutes = '0' + `0${minutes}`.slice(-1) : minutes = minutes;
+        (seconds < 10) ? seconds = `0${seconds}` : seconds = seconds;
+        (hour < 10) ? hour = '0' + `0${hour}`.slice(-1) : hour = hour;
+        heading.textContent = `${hour}hr : ${minutes}min : ${seconds}sec`;
+
+        if (seconds === 59) {
+          seconds = 0;
+          minutes++;
+        }
+        if (minutes === 59) {
+          minutes = 0;
+          hour++;
+        };
+
+      }, 1000);
+    } 
+
+  }else {
+    list.insertAdjacentHTML("beforeend", `<li style='margin: 1rem 1rem 1rem 0;' class='notification is-primary'> ${yourWork} - ${hour}:${minutes}:${seconds}</li>`)
+    button.textContent = 'start';
+    heading.textContent = 'Well Done';
+    clearInterval(timer);
+  }
+});
